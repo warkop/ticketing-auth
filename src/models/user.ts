b13c9ed61a7529fc36ae573a5b285fbe9520dd78
Mohammad Khafid Barokum <khafid@majoo.id>
2021-09-16 22:41:18 +0700

    rename from users.ts to user.ts

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,15 +2,8 @@ import mongoose from 'mongoose';
 import { Password } from '../services/password';
 
 // An interface that describes the properties
-// that are required to create a new User
+// that are requried to create a new User
 interface UserAttrs {
-  email: string;
-  password: string;
-}
-
-// An interface that describes the properties
-// that a User Document has
-interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
@@ -21,18 +14,25 @@ interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
 
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: true
   },
   password: {
     type: String,
-    required: true,
-  },
+    required: true
+  }
 });
 
-userSchema.pre('save', async function passwordHandle(done) {
+userSchema.pre('save', async function(done) {
   if (this.isModified('password')) {
     const hashed = await Password.toHash(this.get('password'));
     this.set('password', hashed);
@@ -40,8 +40,10 @@ userSchema.pre('save', async function passwordHandle(done) {
   done();
 });
 
-const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
 
-userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export { User };
