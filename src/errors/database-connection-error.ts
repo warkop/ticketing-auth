7d35ac2ba01684a67139c57c6b69d3fc2b16bc38
Mohammad Khafid Barokum <khafid@majoo.id>
2021-09-18 06:21:08 +0700

    set indentation

--- src/errors/database-connection-error.ts
+++ src/errors/database-connection-error.ts
@@ -1,17 +1,17 @@
 import { CustomError } from './custom-error';
 
 export class DatabaseConnectionError extends CustomError {
-    statusCode = 500;
+  statusCode = 500;
 
-    reason = 'Error connecting to database';
+  reason = 'Error connecting to database';
 
-    constructor() {
-      super('Error connecting to db');
+  constructor() {
+    super('Error connecting to db');
 
-      Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
-    }
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
 
-    serializeErrors() {
-      return [{ message: this.reason }];
-    }
+  serializeErrors() {
+    return [{ message: this.reason }];
+  }
 }
