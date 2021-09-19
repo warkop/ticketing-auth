c89c8e3d9e61bde892ec1f34b0266795c8bdc5ca
Mohammad Khafid Barokum <khafid@majoo.id>
2021-09-19 13:16:09 +0700

    implement currentUser middleware

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,19 +1,10 @@
-import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
+import express from 'express';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response) => {
-    if (!req.session?.jwt) {
-        return res.send({ currentUser:null })
-    }
-
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        return res.send({currentUser: payload})
-    } catch (err) {
-        return res.send({ currentUser:null })
-    }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+    res.send({ currentUser: req.currentUser || null});
 });
 
 export { router as currentUserRouter };
